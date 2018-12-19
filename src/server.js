ab5972e297341b00f055b0cfc0fa0cd9ac98fc64
Matt Merrill <392778+theothermattm@users.noreply.github.com>
2018-12-19 11:50:35 -0500

    Adding cors

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const logging = require('./logging')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()
 
 app.set('host', process.env.APP_HOST || '0.0.0.0')
 app.set('port', process.env.APP_PORT || '8009')
+app.use(cors())
 app.use(bodyParser.json())
 app.disable('x-powered-by')
 

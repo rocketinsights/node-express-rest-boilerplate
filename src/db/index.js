02304baea280d809e062f7fa8baa188d805a96ec
Matt Merrill <392778+theothermattm@users.noreply.github.com>
2019-01-11 17:04:49 -0500

    Adding mongo connection to startup

--- src/db/index.js
+++ src/db/index.js
@@ -1,2 +1,52 @@
 // FIXME initialize your database here
 // See branches of repo for different skeletons for differing datasources
+
+const mongoose = require('mongoose')
+const logging = require('../logging')
+const Promise = require('bluebird')
+
+const connect = async () => {
+  const connectUri = process.env.DATABASE_CONNECTION_STRING
+  const user = process.env.DATABASE_USER
+  const pass = process.env.DATABASE_PASSWORD
+
+  if (!connectUri || !user || !pass) {
+    logging.log(
+      'fatal',
+      'DATABASE_CONNECTION_STRING, DATABASE_USER and DATABASE_PASSWORD env vars must be set'
+    )
+    process.exit(1)
+  }
+
+  mongoose.Promise = Promise
+  let connectOptions = {
+    // TODO set more appropriate pooling options
+    reconnectInterval: 3000,
+    reconnectTries: 9,
+    poolSize: process.env.DATABASE_MAX_POOL_SIZE || 5
+  }
+  logging.log(
+    'info',
+    'Connecting to mongoose database %s and options %o ...',
+    connectUri,
+    connectOptions
+  )
+  // add these in after so we can safely log connect configurations
+  connectOptions.user = user
+  connectOptions.pass = pass
+  await mongoose.connect(
+    connectUri,
+    connectOptions
+  )
+
+  logging.log('info', 'Connected to mongo successfully')
+
+  mongoose.connection.on('error', error => {
+    logging.log('error', 'Mongoose connection error %o', error)
+  })
+}
+
+
+module.exports = {
+  connect
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@ const dotenv = require('dotenv')
 dotenv.load({ path: '.env' })
 const app = require('./server.js')
 const logging = require('./logging')
-/**
- * Start Express server.
- */
-app.listen(app.get('port'), () => {
-  logging.log('info', 'App is running at http://localhost:%d in %s mode', app.get('port'), app.get('env'))
-  logging.log('info', '  Press CTRL-C to stop\n')
-})
-module.exports = app
+const db = require('../src/db/index')
+
+const startup = async () => {
+  /**
+   * Connect to DB and start express server, in that order.
+   */
+  await db.connect()
+  return new Promise(resolve => {
+    app.listen(app.get('port'), () => {
+      logging.log(
+        'info',
+        'App is running at http://localhost:%d in %s mode',
+        app.get('port'),
+        app.get('env')
+      )
+      logging.log('info', '  Press CTRL-C to stop\n')
+    })
+
+    return resolve(app)
+  })
+}
+
+const returnApp = startup()
+module.exports = returnApp
